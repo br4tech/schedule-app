839ee156c3b1ef849a38068f214d099992daa003
Guilherme Silva <Guilherme.hsilva@ao3tech.com>
2021-01-13 08:50:19 -0300

    Add in app angular-calendar and angular-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContractScheduleComponent } from './schedule/components/contract-schedule/contract-schedule.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContractScheduleComponent
   ],
   imports: [
     BrowserModule,
