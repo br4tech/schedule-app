fbc54b1e042865ebecf23ded2798932d568bd170
Guilherme Silva <Guilherme.hsilva@ao3tech.com>
2021-03-16 15:57:03 -0300

    adjust angular-calendar in app

--- src/app/modules/schedule/schedule.module.ts
+++ src/app/modules/schedule/schedule.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule} from '@angular/common';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 import { ScheduleRoutingModule } from './schedule-routing.module';
 
@@ -8,7 +10,11 @@ import { ContractScheduleComponent } from './components/contract-schedule/contra
 @NgModule({
   imports: [
     CommonModule,
-    ScheduleRoutingModule
+    ScheduleRoutingModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    })
   ],
   declarations: [
     ContractScheduleComponent
