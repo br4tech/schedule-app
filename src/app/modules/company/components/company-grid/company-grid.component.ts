afc110e7e7d03ceadb7d7af0c63ef359907507fd
Guilherme Henrique <guilherme.tr.silva@gmail.com>
2021-07-12 10:25:16 -0300

    feat(component) - Create components to company

---
+++ src/app/modules/company/components/company-grid/company-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-grid',
+  templateUrl: './company-grid.component.html',
+  styleUrls: ['./company-grid.component.scss']
+})
+export class CompanyGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
