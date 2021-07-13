5508712a4b0e07558bf60572144b4df97363d174
Guilherme Henrique <guilherme.tr.silva@gmail.com>
2021-07-12 21:24:05 -0300

    feat(component) - Adjust component to list companies

--- src/app/modules/company/company-list/company-list.component.ts
+++ src/app/modules/company/company-list/company-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-company-list',
@@ -9,9 +11,17 @@ export class CompanyListComponent implements OnInit {
 
   companies : any
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private ngxService: NgxUiLoaderService
+  ) { }
 
   ngOnInit(): void {
+    this.ngxService.start(); 
+    this.activatedRoute.data.subscribe((data) => {
+      this.companies = data.item.companies
+    })
+    this.ngxService.stop();
   }
 
 }
