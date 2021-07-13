4d3937cad12742ee81451d50c3f4558fea382f25
Guilherme Henrique <guilherme.tr.silva@gmail.com>
2021-07-12 23:30:55 -0300

    feat(component) - Adust form t edit and create contract, add nested field to doctors

--- src/app/modules/contract/contract-edit/contract-edit.component.ts
+++ src/app/modules/contract/contract-edit/contract-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contract-edit',
@@ -7,9 +8,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContractEditComponent implements OnInit {
 
-  constructor() { }
+  contractForm: FormGroup;
+
+  data = {
+    doctors: [{
+      doctor_name: "",
+      doctor_document: "",
+      doctor_birthday: "",
+      doctor_crm: "",
+      doctor_email: "",
+      doctor_phone: "",
+      doctor_celphone: "",
+      doctor_speciality:  "",
+    }]
+  }
+
+  constructor(private fb: FormBuilder) {
+    this.contractForm = this.fb.group({
+      name: [''],
+      email: [''],
+      person: [''],
+      document: [''],
+      cep: [''],
+      street: [''],
+      number: [''],
+      complemen: [''],
+      neighborhood: [''],
+      city: [''],
+      state: [''],
+      doctors: this.fb.array([])
+    })
+
+    this.setDoctors();
+   }
 
   ngOnInit(): void {
   }
 
+  get doctorFormGroups () {
+    return this.contractForm.get('doctors') as FormArray
+  }
+
+  addDoctor(){
+    let control = <FormArray>this.contractForm.controls.doctors;
+    control.push(
+      this.fb.group({
+        doctor_name: [''],
+        doctor_document: [''],
+        doctor_birthday: [''],
+        doctor_crm: [''],
+        doctor_email: [''],
+        doctor_phone: [''],
+        doctor_celphone: [''],
+        doctor_speciality: ['']
+      })
+    )
+  }
+
+  deleteDoctor(index: number) {
+    let control = <FormArray>this.contractForm.controls.doctors;
+    control.removeAt(index)
+  }
+
+  setDoctors(){
+    let control = <FormArray>this.contractForm.controls.doctors;
+    this.data.doctors.forEach(f => {
+      control.push(this.fb.group({
+        doctor_name: f.doctor_name,
+        doctor_document:f.doctor_document,
+        doctor_birthday: f.doctor_birthday,
+        doctor_crm: f.doctor_crm,
+        doctor_email: f.doctor_email,
+        doctor_phone: f.doctor_phone,
+        doctor_celphone: f.doctor_celphone,
+        doctor_speciality: f.doctor_speciality
+      }))
+    })
+  }
 }
