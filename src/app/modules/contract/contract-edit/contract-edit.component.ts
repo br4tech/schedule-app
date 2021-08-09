73d2376d1b5d9ef5011910c24e85f726369d55fc
Guilherme Henrique <guilherme.tr.silva@gmail.com>
2021-08-08 22:18:10 -0300

    feat(validators) - Add validation in basic information and FormArray of doctors

--- src/app/modules/contract/contract-edit/contract-edit.component.ts
+++ src/app/modules/contract/contract-edit/contract-edit.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BaseFormComponent } from 'src/app/shared/components/base-form/base-form.component';
+import { FormValidations } from 'src/app/shared/validators/form-validator';
+
 @Component({
   selector: 'app-contract-edit',
   templateUrl: './contract-edit.component.html',
   styleUrls: ['./contract-edit.component.scss']
 })
-export class ContractEditComponent implements OnInit {
+export class ContractEditComponent extends BaseFormComponent implements OnInit {
 
   @ViewChild('ModalCancelAttendanceComponent') openModalAttendance: any;
 
-  contractForm: FormGroup;
+  formulario: FormGroup;
   person_type: string = "Pessoa Juridica"
 
   data = {
@@ -52,22 +55,37 @@ export class ContractEditComponent implements OnInit {
     }]
   }
 
+  PERSON_LIST = [
+    { name: 'Pessoa Juridica', value: '0', checked: true,  },
+    { name: 'Pessoa Fisica', value: '1', checked: false }
+  ]
+
   constructor(
     private fb: FormBuilder  
     ) {
-    this.contractForm = this.fb.group({
-      name: [''],
-      email: [''],
-      person: [''],
-      document: [''],
-      cep: [''],
-      street: [''],
-      number: [''],
+      super();
+   }
+
+  ngOnInit(): void {
+    let getCheckedRadio = null
+    this.PERSON_LIST.forEach(o => {
+      if (o.checked)
+        getCheckedRadio = o.value;
+    });
+
+    this.formulario = this.fb.group({
+      name: ['',  [Validators.required, Validators.minLength(3), Validators.maxLength(35)]],
+      email: ['', [Validators.required, Validators.email]],
+      person: [getCheckedRadio, Validators.required],
+      document: ['', Validators.required],
+      cep: ['', [Validators.required, FormValidations.cepValidator]],
+      street: ['', Validators.required],
+      number: ['', Validators.required],
       complement: [''],
-      neighborhood: [''],
-      city: [''],
-      state: [''],
-      doctors: this.fb.array([]),    
+      neighborhood: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      doctors: this.fb.array([], Validators.required),    
       discounts: this.fb.array([]),
       attendances: this.fb.array([])
     })
@@ -75,18 +93,18 @@ export class ContractEditComponent implements OnInit {
     this.setDoctors();
     this.setDiscounts();
     this.setAttendances();
-   }
-
-  ngOnInit(): void {
   }   
 
+  submit(){
+    // Aqui onde deve passar os dados para o service
+  }
+
   get doctorFormGroups () {
-    return this.contractForm.get('doctors') as FormArray
+    return this.formulario.get('doctors') as FormArray
   }
 
   personType(){
-    debugger;
-    if (this.contractForm.controls.person.value == "0"){
+    if (this.formulario.controls.person.value == "0"){
       this.person_type ="CNPJ"
     }else{
       this.person_type ="CPF"
@@ -94,49 +112,49 @@ export class ContractEditComponent implements OnInit {
   }
 
   addDoctor(){
-    let control = <FormArray>this.contractForm.controls.doctors;
+    let control = <FormArray>this.formulario.controls.doctors;
     control.push(
       this.fb.group({
-        doctor_name: [''],
-        doctor_document: [''],
-        doctor_birthday: [''],
-        doctor_crm: [''],
-        doctor_email: [''],
-        doctor_phone: [''],
+        doctor_name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(35)]],
+        doctor_document: ['', Validators.required],
+        doctor_birthday: ['', Validators.required],
+        doctor_crm: ['', Validators.required ],
+        doctor_email: ['', [Validators.required, Validators.email]],
+        doctor_phone: ['', Validators.required],
         doctor_celphone: [''],
-        doctor_speciality: ['']
+        doctor_speciality: ['', Validators.required]
       })
     )
   }
 
   deleteDoctor(index: number) {
-    let control = <FormArray>this.contractForm.controls.doctors;
+    let control = <FormArray>this.formulario.controls.doctors;
     control.removeAt(index)
   }
 
   setDoctors(){
-    let control = <FormArray>this.contractForm.controls.doctors;
+    let control = <FormArray>this.formulario.controls.doctors;
 
     this.data.doctors.forEach(f => {
       control.push(this.fb.group({
-        doctor_name: f.doctor_name,
-        doctor_document:f.doctor_document,
-        doctor_birthday: f.doctor_birthday,
-        doctor_crm: f.doctor_crm,
-        doctor_email: f.doctor_email,
-        doctor_phone: f.doctor_phone,
+        doctor_name: [f.doctor_name, [Validators.required, Validators.minLength(3), Validators.maxLength(35)]],
+        doctor_document: [f.doctor_document, Validators.required],
+        doctor_birthday: [f.doctor_birthday, Validators.required],
+        doctor_crm: [f.doctor_crm, Validators.required],
+        doctor_email: [f.doctor_email, [Validators.required, Validators.email]],
+        doctor_phone: [f.doctor_phone, Validators.required],
         doctor_celphone: f.doctor_celphone,
-        doctor_speciality: f.doctor_speciality
+        doctor_speciality: [f.doctor_speciality, Validators.required]
       }))
     })
   }
 
   get discountFormGroups () {
-    return this.contractForm.get('discounts') as FormArray
+    return this.formulario.get('discounts') as FormArray
   }
 
   addDiscount(){
-    let control = <FormArray>this.contractForm.controls.discounts;
+    let control = <FormArray>this.formulario.controls.discounts;
     control.push(
       this.fb.group({
         discount_amount: [''],
@@ -148,12 +166,12 @@ export class ContractEditComponent implements OnInit {
   }
 
   deleteDiscount(index: number){
-    let control = <FormArray>this.contractForm.controls.discounts;
+    let control = <FormArray>this.formulario.controls.discounts;
     control.removeAt(index)
   }
 
   setDiscounts(){
-    let control = <FormArray>this.contractForm.controls.discounts;
+    let control = <FormArray>this.formulario.controls.discounts;
     
     this.data.discounts.forEach(f => {
       control.push(this.fb.group({
@@ -166,11 +184,11 @@ export class ContractEditComponent implements OnInit {
   }
 
   get attendanceFormGroups () {
-    return this.contractForm.get('attendances') as FormArray
+    return this.formulario.get('attendances') as FormArray
   }
 
   addAttendance(){
-    let control = <FormArray>this.contractForm.controls.attendances;
+    let control = <FormArray>this.formulario.controls.attendances;
     control.push(
       this.fb.group({
         attendance_kind: [''],
@@ -186,12 +204,12 @@ export class ContractEditComponent implements OnInit {
   }
 
   deleteAttendance(index: number){
-    let control = <FormArray>this.contractForm.controls.attendances;
+    let control = <FormArray>this.formulario.controls.attendances;
     control.removeAt(index)
   }
 
   setAttendances(){
-    let control = <FormArray>this.contractForm.controls.attendances;
+    let control = <FormArray>this.formulario.controls.attendances;
 
     this.data.attendances.forEach(f =>{
       control.push(this.fb.group({
@@ -210,9 +228,4 @@ export class ContractEditComponent implements OnInit {
   openModal(){
     this.openModalAttendance.open('')
   }
-
-  onSubmit() {
-   console.log(this.contractForm.value);
-  }
-
 }
